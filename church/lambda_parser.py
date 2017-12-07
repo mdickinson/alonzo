63d6f6a76e31363d9638e30fa9baed7850c248f9
Mark Dickinson <dickinsm@gmail.com>
2017-12-07 20:38:49 +0000

    Rework shift-reduce parser to cover lambdas as well.

--- church/lambda_parser.py
+++ church/lambda_parser.py
@@ -1,13 +1,8 @@
-"""
+# XXX To investigate: don't push None values onto value stack.
+# XXX To investigate: use lookahead in more places, even when
+#     not strictly necessary, to see if it can reduce the
+#     number of states.
 
-First: without lambdas.
-
-Terminals: ID, LEFT, RIGHT
-
-    atom = ID | LEFT expr RIGHT
-    expr = atom | expr atom
-
-"""
 import string
 
 
@@ -41,19 +36,42 @@ class Name(object):
         )
 
 
+class Function(object):
+    def __init__(self, name, body):
+        self.name = name
+        self.body = body
+
+    def __repr__(self):
+        return "Function({!r}, {!r})".format(self.name, self.body)
+
+    def __eq__(self, other):
+        return (
+            type(self) == type(other)
+            and self.name == other.name
+            and self.body == other.body
+        )
+
+
+# Token types: terminals
 ID = "id"
 LEFT = "left"
 RIGHT = "right"
-LAMBDA = "lambda"
+SLASH = "slash"
 DOT = "dot"
 END = "end"
+
+# Token types: non-terminals
+ATOM = "atom"
+EXPR = "expr"
+NAMES = "names"
+
 
 IDENTIFIER_CHARACTERS = set(string.ascii_lowercase + "_")
 WHITESPACE = set(" \n")
 SINGLE_TOKEN_CHARS = {
     "(": LEFT,
     ")": RIGHT,
-    "\\": LAMBDA,
+    "\\": SLASH,
     ".": DOT,
 }
 
@@ -99,130 +117,316 @@ class TokenStream(object):
     """
     def __init__(self, tokens):
         self._tail = iter(tokens)
-        # We assume that `None` is not a valid token.
-        self._head = None
+        self._head = []
 
     def next(self):
-        if self._head is None:
-            token = next(self._tail)
-        else:
-            token, self._head = self._head, None
-        return token
+        return self._head.pop(0) if self._head else next(self._tail)
 
     def push(self, token):
-        if self._head is None:
-            self._head = token
-        else:
-            raise ValueError("no space to push")
+        self._head.insert(0, token)
+
+    def peek(self):
+        token = self.next()
+        self.push(token)
+        return token
+
+    def peek_type(self):
+        token_type, token_value = self.peek()
+        return token_type
+
+
+# Parser states.
+BEGIN = "BEGIN"
+BEGIN_EXPR = "BEGIN_EXPR"
+BEGIN_EXPR_END = "BEGIN_EXPR_END"
+EXPR_ATOM = "EXPR_ATOM"
+SLEFT = "LEFT"
+SLEFT_EXPR = "LEFT_EXPR"
+SLEFT_EXPR_RIGHT = "LEFT_EXPR_RIGHT"
+SATOM = "ATOM"
+SID = "SID"
+SSLASH = "SLASH"
+SNAMES = "NAMES"
+SSLASH_NAMES = "SLASH_NAMES"
+SLAMBDA = "LAMBDA"
+RLAMBDA = "RLAMBDA"
+SNAMES_ID = "SNAMES_ID"
+SLAMBDA_EXPR = "LAMBDA_EXPR"
 
 
 class SMParser(object):
     """State-machine-based shift-reduce parser."""
-    def __init__(self, transitions, reductions, initial_state, accept_state):
-        self.transitions = transitions
-        self.reductions = reductions
-        self.accept_state = accept_state
-        self.initial_state = initial_state
 
     def parse(self, tokens):
         tokens = TokenStream(tokens)
         value_stack = []
         state_stack = []
-        state = self.initial_state
-        while state != self.accept_state:
-            if state in self.transitions:
+        state = BEGIN
+
+        def shift(next_state):
+            state_stack.append(state)
+            value_stack.append(token_value)
+            return next_state
+
+        while True:
+            if state == BEGIN:
                 token_type, token_value = tokens.next()
-                try:
-                    next_state = self.transitions[state][token_type]
-                except KeyError:
+                if token_type == ID:
+                    state = shift(SID)
+                elif token_type == LEFT:
+                    state = shift(SLEFT)
+                elif token_type == RIGHT:
                     raise ParseError()
-                value_stack.append(token_value)
-                state_stack.append(state)
-                state = next_state
-            elif state in self.reductions:
-                count, type, reducer = self.reductions[state]
-                args, value_stack = value_stack[-count:], value_stack[:-count]
-                state, state_stack = state_stack[-count], state_stack[:-count]
-                token = type, reducer(*args)
-                tokens.push(token)
+                elif token_type == SLASH:
+                    state = shift(SSLASH)
+                elif token_type == DOT:
+                    raise ParseError()
+                elif token_type == END:
+                    raise ParseError()
+
+                elif token_type == ATOM:
+                    state = shift(SATOM)
+                elif token_type == EXPR:
+                    state = shift(BEGIN_EXPR)
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == BEGIN_EXPR:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SID)
+                elif token_type == LEFT:
+                    state = shift(SLEFT)
+                elif token_type == RIGHT:
+                    raise ParseError()
+                elif token_type == SLASH:
+                    state = shift(SSLASH)
+                elif token_type == DOT:
+                    raise ParseError()
+                elif token_type == END:
+                    state = shift(BEGIN_EXPR_END)
+
+                elif token_type == ATOM:
+                    state = shift(EXPR_ATOM)
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SLEFT:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SID)
+                elif token_type == LEFT:
+                    state = shift(SLEFT)
+                elif token_type == RIGHT:
+                    raise ParseError()
+                elif token_type == SLASH:
+                    state = shift(SSLASH)
+                elif token_type == DOT:
+                    raise ParseError()
+                elif token_type == END:
+                    raise ParseError()
+
+                elif token_type == ATOM:
+                    state = shift(SATOM)
+                elif token_type == EXPR:
+                    state = shift(SLEFT_EXPR)
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SLEFT_EXPR:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SID)
+                elif token_type == LEFT:
+                    state = shift(SLEFT)
+                elif token_type == RIGHT:
+                    state = shift(SLEFT_EXPR_RIGHT)
+                elif token_type == SLASH:
+                    state = shift(SSLASH)
+                elif token_type == DOT:
+                    raise ParseError()
+                elif token_type == END:
+                    raise ParseError()
+
+                elif token_type == ATOM:
+                    state = shift(EXPR_ATOM)
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SSLASH:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SNAMES)
+                elif token_type == LEFT:
+                    raise ParseError()
+                elif token_type == RIGHT:
+                    raise ParseError()
+                elif token_type == SLASH:
+                    raise ParseError()
+                elif token_type == DOT:
+                    raise ParseError()
+                elif token_type == END:
+                    raise ParseError()
+
+                elif token_type == NAMES:
+                    state = shift(SSLASH_NAMES)
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SSLASH_NAMES:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SNAMES_ID)
+                elif token_type == LEFT:
+                    raise ParseError()
+                elif token_type == RIGHT:
+                    raise ParseError()
+                elif token_type == SLASH:
+                    raise ParseError()
+                elif token_type == DOT:
+                    state = shift(SLAMBDA)
+                elif token_type == END:
+                    raise ParseError()
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SLAMBDA:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SID)
+                elif token_type == LEFT:
+                    state = shift(SLEFT)
+                elif token_type == RIGHT:
+                    raise ParseError()
+                elif token_type == SLASH:
+                    state = shift(SSLASH)
+                elif token_type == DOT:
+                    raise ParseError()
+                elif token_type == END:
+                    raise ParseError()
+
+                elif token_type == ATOM:
+                    state = shift(SATOM)
+                elif token_type == EXPR:
+                    # Need lookahead!
+                    if tokens.peek_type() in {END, RIGHT}:
+                        state = shift(RLAMBDA)
+                    else:
+                        state = shift(SLAMBDA_EXPR)
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SLAMBDA_EXPR:
+                token_type, token_value = tokens.next()
+                if token_type == ID:
+                    state = shift(SID)
+                elif token_type == LEFT:
+                    state = shift(SLEFT)
+                elif token_type == SLASH:
+                    state = shift(SSLASH)
+                elif token_type == DOT:
+                    raise ParseError()
+                # Not possible to have RIGHT or END here, since
+                # we can only reach this state from SLAMBDA, after
+                # lookahead.
+
+                elif token_type == ATOM:
+                    state = shift(EXPR_ATOM)
+
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
+
+            elif state == SNAMES_ID:
+                # Reduce: names -> names ID
+                state, state_stack = state_stack[-2], state_stack[:-2]
+                values, value_stack = value_stack[-2:], value_stack[:-2]
+                names, name = values
+                names.append(name)
+                tokens.push((NAMES, names))
+            elif state == RLAMBDA:
+                # Reduce: atom -> SLASH names DOT expr
+                state, state_stack = state_stack[-4], state_stack[:-4]
+                values, value_stack = value_stack[-4:], value_stack[:-4]
+                _, names, _, atom = values
+                while names:
+                    atom = Function(names.pop(), atom)
+                tokens.push((ATOM, atom))
+            elif state == SNAMES:
+                # Reduce: names -> ID
+                state, state_stack = state_stack[-1], state_stack[:-1]
+                value, value_stack = value_stack[-1], value_stack[:-1]
+                names = [value]
+                tokens.push((NAMES, names))
+            elif state == SID:
+                # Reduce: atom -> ID
+                state, state_stack = state_stack[-1], state_stack[:-1]
+                value, value_stack = value_stack[-1], value_stack[:-1]
+                atom = Name(value)
+                tokens.push((ATOM, atom))
+            elif state == SATOM:
+                # Reduce: expr -> atom
+                state, state_stack = state_stack[-1], state_stack[:-1]
+                value, value_stack = value_stack[-1], value_stack[:-1]
+                expr = value
+                tokens.push((EXPR, expr))
+            elif state == EXPR_ATOM:
+                # Reduce: expr -> expr atom
+                state, state_stack = state_stack[-2], state_stack[:-2]
+                values, value_stack = value_stack[-2:], value_stack[:-2]
+                expr = Apply(values[0], values[1])
+                tokens.push((EXPR, expr))
+            elif state == SLEFT_EXPR_RIGHT:
+                # Reduce: expr -> LEFT expr RIGHT
+                state, state_stack = state_stack[-3], state_stack[:-3]
+                value, value_stack = value_stack[-2], value_stack[:-3]
+                atom = value
+                tokens.push((ATOM, atom))
+            elif state == BEGIN_EXPR_END:
+                # Accept.
+                expr, _ = value_stack
+                return expr
             else:
-                raise RuntimeError("Unknown state: {!r}".format(state))
-        return value_stack.pop()
-
-
-BEGIN = "begin"
-EXPR = "expr"
-GOAL = "goal"
-
-"""
-States and transitions (symbols are ID, LEFT, RIGHT, END, EXPR)
-
-BEGIN:
-    LEFT -> shift to LEFT
-    ID -> shift to ID
-    EXPR -> shift to BEGIN_EXPR
-    GOAL -> shift to ACCEPT
-LEFT:
-    LEFT -> shift to LEFT
-    ID -> shift to ID
-    EXPR -> shift to LEFT_EXPR
-BEGIN_EXPR:
-    LEFT -> shift to LEFT
-    ID -> shift to ID
-    EXPR -> shift to EXPR_EXPR
-    END -> shift to BEGIN_EXPR_END
-LEFT_EXPR:
-    LEFT -> shift to LEFT
-    ID -> shift to ID
-    EXPR -> shift to EXPR_EXPR
-    RIGHT -> shift to LEFT_EXPR_RIGHT
-
-ID:
-    reduce (expr -> id)
-EXPR_EXPR:
-    reduce (expr -> expr expr)
-LEFT_EXPR_RIGHT:
-    reduce (expr -> (expr) )
-BEGIN_EXPR_END:
-    reduce (goal -> expr END)
-
-Need a token stack with push-back, though we only ever need one
-token of push back.
-"""
-
-ACCEPT = "accept"
-BEGIN_EXPR = (BEGIN, EXPR)
-LEFT_EXPR = (LEFT, EXPR)
-EXPR_EXPR = (EXPR, EXPR)
-BEGIN_EXPR_END = (BEGIN, EXPR, END)
-LEFT_EXPR_RIGHT = (LEFT, EXPR, END)
-
-# Transition table for shift states.
-transitions = {
-    BEGIN: {LEFT: LEFT, ID: ID, EXPR: BEGIN_EXPR, GOAL: ACCEPT},
-    LEFT: {LEFT: LEFT, ID: ID, EXPR: LEFT_EXPR},
-    BEGIN_EXPR: {LEFT: LEFT, ID: ID, EXPR: EXPR_EXPR, END: BEGIN_EXPR_END},
-    LEFT_EXPR: {LEFT: LEFT, ID: ID, EXPR: EXPR_EXPR, RIGHT: LEFT_EXPR_RIGHT},
-}
-
-# Reduction states: each state maps to the number of values it
-# consumes followed by the type of token produced by the reduction
-# and the function.
-reductions = {
-    ID: (1, EXPR, Name),
-    EXPR_EXPR: (2, EXPR, Apply),
-    LEFT_EXPR_RIGHT: (3, EXPR, lambda x, y, z: y),
-    BEGIN_EXPR_END: (2, GOAL, lambda x, y: x),
-}
-
-
-parser = SMParser(
-    transitions=transitions,
-    reductions=reductions,
-    initial_state=BEGIN,
-    accept_state=ACCEPT,
-)
+                token_type, token_value = tokens.next()
+                if False:
+                    pass
+                else:
+                    raise NotImplementedError(
+                        "token type {!r} in state {!r}".format(
+                            token_type, state))
 
 
 def parse(s):
-    return parser.parse(tokenize(s))
+    tokens = tokenize(s)
+    return SMParser().parse(tokens)
+
+
+"""
+Grammar
+
+   names -> ID | names ID
+   atom -> ID | LEFT expr RIGHT | SLASH names DOT expr
+   expr -> atom | expr atom
+
+"""
--- church/test/test_lambda_parser.py
+++ church/test/test_lambda_parser.py
@@ -1,23 +1,42 @@
 import unittest
 
-from church.lambda_parser import Apply, Name, parse, ParseError
+from church.lambda_parser import Apply, Function, Name, parse, ParseError
 
 
 class TestLambdaParser(unittest.TestCase):
     def test_parse_simple_expressions(self):
         w, x, y, z = map(Name, "wxyz")
-        self.assertEqual(parse("x"), x)
-        self.assertEqual(parse("x y"), Apply(x, y))
-        self.assertEqual(parse("x (y)"), Apply(x, y))
-        self.assertEqual(parse("(x) y"), Apply(x, y))
-        self.assertEqual(parse("(x)"), x)
-        self.assertEqual(parse("((x))"), x)
-        self.assertEqual(parse("(((x)))"), x)
-        self.assertEqual(parse("x y z"), Apply(Apply(x, y), z))
-        self.assertEqual(parse("(x y) z"), Apply(Apply(x, y), z))
-        self.assertEqual(parse("x (y z)"), Apply(x, Apply(y, z)))
-        self.assertEqual(parse("w x y z"), Apply(Apply(Apply(w, x), y), z))
-        self.assertEqual(parse("(w x)(y z)"), Apply(Apply(w, x), Apply(y, z)))
+        test_pairs = [
+            ("x", x),
+            ("x y", Apply(x, y)),
+            ("x (y)", Apply(x, y)),
+            ("(x (y))", Apply(x, y)),
+            ("(x) y", Apply(x, y)),
+            ("(x)", x),
+            ("((x))", x),
+            ("(((x)))", x),
+            ("x y z", Apply(Apply(x, y), z)),
+            ("(x y) z", Apply(Apply(x, y), z)),
+            ("x (y z)", Apply(x, Apply(y, z))),
+            ("w x y z", Apply(Apply(Apply(w, x), y), z)),
+            ("(w x)(y z)", Apply(Apply(w, x), Apply(y, z))),
+
+            (r"\x.x", Function("x", x)),
+            (r"\x y.x", Function("x", Function("y", x))),
+            (r"(\x y.x)", Function("x", Function("y", x))),
+            (r"\x.x(y)", Function("x", Apply(x, y))),
+            (r"\x.x \y.y", Function("x", Apply(x, Function("y", y)))),
+            (r"\x y.x y", Function("x", Function("y", Apply(x, y)))),
+            (r"\x.\y.x", Function("x", Function("y", x))),
+            (r"\x y.(x)", Function("x", Function("y", x))),
+            (r"\x.\y.x y", Function("x", Function("y", Apply(x, y)))),
+            (r"\x.(\y.x) y", Function("x", Apply(Function("y", x), y))),
+            (r"(\x.\y.x) y", Apply(Function("x", Function("y", x)), y)),
+            (r"x \y. x", Apply(x, Function("y", x))),
+            (r"(x \y. x)", Apply(x, Function("y", x))),
+        ]
+        for code, expr in test_pairs:
+            self.assertEqual(parse(code), expr)
 
     def test_parse_errors(self):
         bad_strings = [
@@ -26,10 +45,26 @@ class TestLambdaParser(unittest.TestCase):
             ")",
             "()",
             "(x",
-            "x (",
-            "x )",
+            "(x.",
+            "x)",
+            "x(",
+            ")x",
             ".",
+            "x.",
+            "(.",
+            r"\ ",
+            r"\\ ",
+            r"\(",
+            r"\.",
+            r"\)",
             r"\x x",
+            r"\x\ ",
+            r"\x(",
+            r"\x)",
+            r"\x.)",
+            r"\x..",
+            r"\x.",
+            r"\x. x.",
         ]
         for bad_string in bad_strings:
             with self.assertRaises(ParseError):
