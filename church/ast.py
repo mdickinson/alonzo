74aebba4a41cbcfa560ec05dce7f609e416340f7
Mark Dickinson <dickinsm@gmail.com>
2017-12-20 14:22:55 +0000

    Refactor: move AST parsing and unparsing into a single module.

--- church/ast.py
+++ church/ast.py
@@ -1,4 +1,12 @@
-from church.token import Token, tokenize, TokenType
+import enum
+
+from church.token import (
+    END_TOKEN,
+    ID_TOKEN,
+    SINGLE_CHAR_TOKEN,
+    Token,
+    TokenType,
+)
 
 
 # Classes providing the AST for the parsed expressions.
@@ -187,5 +195,66 @@ class LambdaParser(object):
             return self.values.pop()
 
 
-def parse(s):
-    return LambdaParser(TokenStream(tokenize(s))).parse_expr()
+def parse(tokens):
+    return LambdaParser(TokenStream(tokens)).parse_expr()
+
+
+class UnparseState(enum.Enum):
+    """
+    States used by unparse.
+    """
+    TOP = enum.auto()
+    LEADING = enum.auto()
+    TRAILING = enum.auto()
+    MIDDLE = enum.auto()
+    WRITE_LEFT = enum.auto()
+    WRITE_RIGHT = enum.auto()
+
+
+#: Map from old to new state for Function arguments.
+ARGUMENT_STATE = {
+    UnparseState.TOP: UnparseState.TRAILING,
+    UnparseState.LEADING: UnparseState.MIDDLE,
+}
+
+
+def unparse(expr):
+    """
+    Unparse an expr into a sequence of tokens.
+    """
+    # to_do maintains a list of (state, expr) pairs.
+    # For some states, the corresponding expr is None.
+    to_do = [(UnparseState.TOP, expr)]
+    while to_do:
+        state, expr = to_do.pop()
+        if state == UnparseState.WRITE_RIGHT:
+            yield SINGLE_CHAR_TOKEN[")"]
+        elif state == UnparseState.WRITE_LEFT:
+            yield SINGLE_CHAR_TOKEN["("]
+        elif type(expr) == Name:
+            yield ID_TOKEN(expr.name)
+        elif type(expr) == Apply:
+            if state in ARGUMENT_STATE:
+                to_do.append((ARGUMENT_STATE[state], expr.argument))
+                to_do.append((UnparseState.LEADING, expr.function))
+            else:
+                to_do.append((UnparseState.WRITE_RIGHT, None))
+                to_do.append((UnparseState.TOP, expr))
+                to_do.append((UnparseState.WRITE_LEFT, None))
+        elif type(expr) == Function:
+            if state in {UnparseState.TOP, UnparseState.TRAILING}:
+                body, names = expr, []
+                while type(body) == Function:
+                    names.append(body.name)
+                    body = body.body
+                to_do.append((UnparseState.TOP, body))
+                yield SINGLE_CHAR_TOKEN["\\"]
+                yield from (ID_TOKEN(name) for name in names)
+                yield SINGLE_CHAR_TOKEN["."]
+            else:
+                to_do.append((UnparseState.WRITE_RIGHT, None))
+                to_do.append((UnparseState.TOP, expr))
+                to_do.append((UnparseState.WRITE_LEFT, None))
+        else:
+            assert False, "should never get here"
+    yield END_TOKEN
--- church/test/test_ast.py
+++ church/test/test_ast.py
@@ -1,9 +1,18 @@
 import unittest
 
-from church.lambda_parser import Apply, Function, Name, parse, ParseError
+from church.ast import Apply, Function, Name, parse, ParseError, unparse
+from church.token import END_TOKEN, ID_TOKEN, SINGLE_CHAR_TOKEN, tokenize
+
+#: Shortcuts for ease of testing.
+LEFT = SINGLE_CHAR_TOKEN["("]
+RIGHT = SINGLE_CHAR_TOKEN[")"]
+SLASH = SINGLE_CHAR_TOKEN["\\"]
+DOT = SINGLE_CHAR_TOKEN["."]
+ID = ID_TOKEN
+END = END_TOKEN
 
 
-class TestLambdaParser(unittest.TestCase):
+class TestParse(unittest.TestCase):
     def test_parse_simple_expressions(self):
         w, x, y, z = map(Name, "wxyz")
         test_pairs = [
@@ -38,7 +47,8 @@ class TestLambdaParser(unittest.TestCase):
             (r"(x\y.y)z", Apply(Apply(x, Function("y", y)), z)),
         ]
         for code, expr in test_pairs:
-            self.assertEqual(parse(code), expr)
+            with self.subTest(code):
+                self.assertEqual(parse(tokenize(code)), expr)
 
     def test_deeply_nested_constructs(self):
         # Check for performance problems and uses of Python recursion.
@@ -50,35 +60,35 @@ class TestLambdaParser(unittest.TestCase):
         expected = x
         for _ in range(repeats):
             expected = Apply(expected, x)
-        self.assertEqual(parse(code), expected)
+        self.assertEqual(parse(tokenize(code)), expected)
 
         # Pattern: "(((x)x)x)x"
         code = "("*repeats + "x" + ")x"*repeats
         expected = x
         for _ in range(repeats):
             expected = Apply(expected, x)
-        self.assertEqual(parse(code), expected)
+        self.assertEqual(parse(tokenize(code)), expected)
 
         # Pattern: "x(x(x(x)))
         code = "x("*repeats + "x" + ")"*repeats
         expected = x
         for _ in range(repeats):
             expected = Apply(x, expected)
-        self.assertEqual(parse(code), expected)
+        self.assertEqual(parse(tokenize(code)), expected)
 
         # Pattern: "\x.\x.\x.x"
         code = r"\x."*repeats + "x"
         expected = x
         for _ in range(repeats):
             expected = Function("x", expected)
-        self.assertEqual(parse(code), expected)
+        self.assertEqual(parse(tokenize(code)), expected)
 
         # Pattern: "\x x x.x".
         code = r"\x" + " x"*(repeats-1) + ".x"
         expected = x
         for _ in range(repeats):
             expected = Function("x", expected)
-        self.assertEqual(parse(code), expected)
+        self.assertEqual(parse(tokenize(code)), expected)
 
     def test_parse_errors(self):
         bad_strings = [
@@ -111,4 +121,32 @@ class TestLambdaParser(unittest.TestCase):
         ]
         for bad_string in bad_strings:
             with self.assertRaises(ParseError, msg=repr(bad_string)):
-                parse(bad_string)
+                parse(tokenize(bad_string))
+
+
+class TestUnparse(unittest.TestCase):
+    def test_nonclosed_expressions(self):
+        X = ID("x")
+        pairs = [
+            ("x", [X, END]),
+            ("x x", [X, X, END]),
+            ("x x x", [X, X, X, END]),
+            ("x(x x)", [X, LEFT, X, X, RIGHT, END]),
+            ("((x x)x)x", [X, X, X, X, END]),
+            ("x(x(x x))", [X, LEFT, X, LEFT, X, X, RIGHT, RIGHT, END]),
+            ("(x(x x))x", [X, LEFT, X, X, RIGHT, X, END]),
+            ("(x x)(x x)", [X, X, LEFT, X, X, RIGHT, END]),
+            ("x(x x x)", [X, LEFT, X, X, X, RIGHT, END]),
+            (r"\x.x", [SLASH, X, DOT, X, END]),
+            (r"\x.x x", [SLASH, X, DOT, X, X, END]),
+            (r"(\x.x)x", [LEFT, SLASH, X, DOT, X, RIGHT, X, END]),
+            (r"x \x.x", [X, SLASH, X, DOT, X, END]),
+            (r"x(\x.x)x", [X, LEFT, SLASH, X, DOT, X, RIGHT, X, END]),
+            (r"\x x.x x", [SLASH, X, X, DOT, X, X, END]),
+        ]
+
+        for test_expr, expected_tokens in pairs:
+            with self.subTest(test_expr):
+                expr = parse(tokenize(test_expr))
+                actual_tokens = list(unparse(expr))
+                self.assertEqual(actual_tokens, expected_tokens)
